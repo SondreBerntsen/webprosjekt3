501b9f508c20d613ddf87e88276d9744af9a7099
Mads <mmoigaar@stud.ntnu.no>
2018-10-24 15:01:41 +0200

    program page things

---
+++ react-app/src/components/programme/MinEventCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const MinEventCard = (props) => {
+  console.log(props);
+  return (  
+    <div className="card">
+      <div className="card-body">
+        <div>
+          <p className="event_title">{props.event.title}</p>
+          <p className="venue_name">{props.event.venue}</p>
+        </div>
+        <p className="time">{props.event.time}</p>
+        <p className="price">{props.event.price}</p>
+        <div className="buy_ticket">
+          <p>Kjøp billetter</p>
+        </div>
+        <div className="more_info">
+          <p>Mer info</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default MinEventCard;
