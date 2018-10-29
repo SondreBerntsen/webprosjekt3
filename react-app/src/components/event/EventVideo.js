97461694b23f55852b7727f84ff08f140f95e34a
elissi <elisamed@stud.ntnu.no>
2018-10-29 20:38:16 +0100

    added react-youtube and put in API

--- react-app/src/components/event/EventVideo.js
+++ react-app/src/components/event/EventVideo.js
@@ -1,19 +1,28 @@
 //EventVideo page
-import React from "react";
 
+//https://www.npmjs.com/package/react-youtube
+import React, { Component } from "react";
+import YouTube from "react-youtube";
 
-const EventVideo = ({ url }) => {
+class EventVideo extends Component {
+  render() {
+    const url = this.props.url;
+    const opts = {
+      height: "390",
+      width: "640",
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
 
+    return <YouTube videoId={url} opts={opts} onReady={this._onReady} />;
+  }
 
-    return (
-        <div>
-            <iframe id="ytplayer" type="text/html" width="640" height="360"
-                src={url}
-            ></iframe>
-        </div>
-    )
-
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo(); //playVideo
+  }
 }
-
 
 export default EventVideo;
