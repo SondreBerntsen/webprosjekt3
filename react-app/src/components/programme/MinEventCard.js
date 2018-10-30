1e2a24c3f8939ba4f04cee4116940b22bff70115
elissi <elisamed@stud.ntnu.no>
2018-10-31 00:57:04 +0100

    function checkUrl in MinEventCard.js component

--- react-app/src/components/programme/MinEventCard.js
+++ react-app/src/components/programme/MinEventCard.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const MinEventCard = (props) => {
-
+const MinEventCard = props => {
+  // Outputs "More info" div only if the user is on Programme page
+  function checkUrl() {
+    if (window.location.href.indexOf("program") > -1) {
+      return (
+        <React.Fragment>
+          <div className="col-md-3">
+            <button className="btn btn-outline-info infoBtn">Mer info</button>
+          </div>
+        </React.Fragment>
+      );
+    }
+  }
   // Outputs "buy tickets" div only if the event is not free
   function checkPrice() {
     if (props.event.price !== 0) {
@@ -9,7 +20,9 @@ const MinEventCard = (props) => {
         <React.Fragment>
           <p className="price col-md-3">{props.event.price}</p>
           <div className=" col-md-3">
-            <button className="btn btn-outline-success buy_ticketBtn">Kjøp billetter</button>
+            <button className="btn btn-outline-success buy_ticketBtn">
+              Kjøp billetter
+            </button>
           </div>
         </React.Fragment>
       );
@@ -17,8 +30,8 @@ const MinEventCard = (props) => {
       return (
         <React.Fragment>
           <p className="price col-md-3">"GRATIS"</p>
-          <span className="col-md-3"></span>
-        </React.Fragment >
+          <span className="col-md-3" />
+        </React.Fragment>
       );
     }
   }
@@ -31,16 +44,18 @@ const MinEventCard = (props) => {
       </div>
       <div className="col-md-8 infoEvent">
         <div className="row">
-          <p className="time col-md-3"><span role="img" aria-label="icon">&#128337;</span> {props.event.time}</p>
+          <p className="time col-md-3">
+            <span role="img" aria-label="icon">
+              &#128337;
+            </span>{" "}
+            {props.event.time}
+          </p>
           {checkPrice()}
-          <div className="col-md-3">
-            <button className="btn btn-outline-info infoBtn">Mer info</button>
-          </div>
+          {checkUrl()}
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default MinEventCard;
