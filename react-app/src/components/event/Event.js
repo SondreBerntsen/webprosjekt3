26e4ad477de466eb8ca0179d9812e8ccd58b9948
elissi <elisamed@stud.ntnu.no>
2018-10-30 13:48:31 +0100

    eventVideo with placeholder image over video

--- react-app/src/components/event/Event.js
+++ react-app/src/components/event/Event.js
@@ -2,20 +2,32 @@
 //mye fix her når database kommer
 import React, { Component } from "react";
 import Navbar from "../Navbar";
+import MinEventCard from "../programme/MinEventCard";
 import EventVideo from "./EventVideo";
 import "../../styles/events.css";
 
 class Event extends Component {
   state = {
     id: null,
+
+    events: [
+      {
+        eventid: 1,
+        title: "ViolinKveld med kattepusene",
+        venue: "Scene 1",
+        time: "20:00",
+        price: "NOK 100,-"
+      }
+    ],
+
     event: {
       eventImg:
         "https://static1.squarespace.com/static/57242faa7da24f738c3b9c32/5b736843352f53e1d56a8215/5b73684840ec9a45a9538c78/1534289997493/_DSC9283.jpg?format=1000w",
       eventTitle: "ViolinKveld med kattepusene",
       eventDate: "Mandag 21.09.2018",
       eventTxt:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      eventVideoURL: "2g811Eo7K8U"
+        "Her har Ragnhild en etterlengtet kveld med kattepusene, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      eventVideoURL: "Y2HJpa0FU88"
     }
   };
   componentDidMount() {
@@ -25,17 +37,20 @@ class Event extends Component {
     });
   }
   render() {
+    //console.log(this.state.id);
     const event = this.state.event ? (
       <div className="container">
         <h2>{this.state.event.eventTitle}</h2>
         <p className="eventDate">{this.state.event.eventDate}</p>
-        <img
-          className="eventImg"
-          src={this.state.event.eventImg}
-          alt="eventimg"
+        <EventVideo
+          url={this.state.event.eventVideoURL}
+          title={this.state.event.eventTitle}
+          placeholderImage={this.state.event.eventImg}
         />
+
+        <MinEventCard key={this.state.id} event={this.state.events[0]} />
+
         <p>{this.state.event.eventTxt}</p>
-        <EventVideo url={this.state.event.eventVideoURL} />
       </div>
     ) : (
       <div>Error melding her.. fix video 31</div>
@@ -43,7 +58,6 @@ class Event extends Component {
     return (
       <div>
         <Navbar />
-        <span> id = {this.state.id}</span>
         {event}
       </div>
     );
--- react-app/src/components/event/EventVideo.js
+++ react-app/src/components/event/EventVideo.js
@@ -1,27 +1,117 @@
 //EventVideo page
 
-//https://www.npmjs.com/package/react-youtube
+//https://codepen.io/vincentorback/pen/KaqbLK
 import React, { Component } from "react";
-import YouTube from "react-youtube";
+import "../../styles/eventvideo.css";
 
 class EventVideo extends Component {
   render() {
-    const url = this.props.url;
-    const opts = {
-      height: "390",
-      width: "640",
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1
+    const url = `https://www.youtube.com/embed/${
+      this.props.url
+    }?feature=oembed&autoplay=1&hd=1`;
+    const videoElements = document.querySelectorAll(".js-video");
+    const activeClass = "is-active";
+    const loadingClass = "is-loading";
+    const videoWrapSelector = ".js-videoWrap";
+    const videoPlaceholderSelector = ".js-videoPlaceholder";
+
+    function loadVideo(event) {
+      let video = event.currentTarget;
+      video.removeEventListener("click", loadVideo, false);
+      video.classList.add(activeClass, loadingClass);
+
+      let wrap = video.querySelector(videoWrapSelector);
+      let placeholder = video.querySelector(videoPlaceholderSelector);
+      let videoUrl = video.getAttribute("data-url");
+      let videoClasses = video.getAttribute("data-class");
+
+      let iframe = stringToElements(
+        `<iframe
+      class="${videoClasses}"
+      src="${videoUrl}"
+      frameborder="0"
+      allowfullscreen
+    />`
+      );
+
+      iframe.onload = () => {
+        video.classList.remove(loadingClass);
+        window.setTimeout(() => {
+          placeholder.parentNode.removeChild(placeholder);
+        });
+      };
+
+      wrap.appendChild(iframe);
+    }
+
+    Array.from(videoElements, video => {
+      video.addEventListener("click", loadVideo, false);
+    });
+
+    function stringToElements(string) {
+      const div = document.createElement("div");
+      div.innerHTML = string;
+
+      if (div.childNodes.length > 1) {
+        return div.childNodes;
       }
-    };
 
-    return <YouTube videoId={url} opts={opts} onReady={this._onReady} />;
-  }
+      return div.firstChild;
+    }
+    /*checks if event has youtube link or not*/
+    const hasVideo =
+      this.props.url !== "" ? (
+        /*if event has youtube link*/
+        <div>
+          <div
+            className="Video js-video"
+            data-url={url}
+            data-class="Video-iframe"
+            data-width="1280"
+            data-height="720"
+          >
+            <div className="Video-wrap js-videoWrap">
+              <a className="Video-title" href="full-url-goes-here">
+                {this.props.title}
+              </a>
+              <img
+                className="Video-placeholder js-videoPlaceholder"
+                src={this.props.placeholderImage}
+                alt=""
+              />
+              <svg
+                className="Video-playButton"
+                viewBox="0 0 136 198"
+                width="50"
+                height="80"
+              >
+                <polygon fill="currentColor" points="136 102 0 198 0 0" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      ) : (
+        /*if the event has no youtube link*/
+        <div>
+          <div data-width="1280" data-height="720">
+            <div className="Video-wrap js-videoWrap">
+              <img
+                className="Video-placeholder js-videoPlaceholder"
+                src={this.props.placeholderImage}
+                alt=""
+              />
+              <svg
+                className="Video-playButton"
+                viewBox="0 0 136 198"
+                width="50"
+                height="80"
+              />
+            </div>
+          </div>
+        </div>
+      );
 
-  _onReady(event) {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo(); //playVideo
+    return <div>{hasVideo}</div>;
   }
 }
 
