003f73cd75f2abf1968b17db97cbda83b23124e9
elissi <elisamed@stud.ntnu.no>
2018-11-24 00:29:54 +0100

    functions for AdminAbout finished

--- express/about.js
+++ express/about.js
@@ -1,12 +1,12 @@
+// about.js
 const express = require("express");
 var db = require("./db");
 const about = express.Router();
 
-const SELECT_ALL_GENERAL_QUERY =
-  "SELECT id, pitch, dateHeader_txt, address, vision_txt, partner_txt_official, partner_txt_private, organization_txt FROM general";
-
 about.get("/", (req, res) => {
-  db.query(SELECT_ALL_GENERAL_QUERY, (err, results) => {
+  let SELECT_QUERY =
+    "SELECT id, pitch, dateHeader_txt, address, vision_txt, partner_txt_official, partner_txt_private, organization_txt FROM general";
+  db.query(SELECT_QUERY, (err, results) => {
     if (err) {
       return res.send(err);
     } else {
@@ -14,6 +14,20 @@ about.get("/", (req, res) => {
         data: results
       });
     }
+  });
+});
+
+about.post("/frontpageUpdate", (req, res) => {
+  let { vision_txt, organization_txt, address } = req.body;
+  let UPDATE_QUERY = `
+  UPDATE general
+  SET 
+    vision_txt = '${vision_txt}',
+    organization_txt = '${organization_txt}',
+    address = '${address}'`;
+  db.query(UPDATE_QUERY, (err, results) => {
+    if (err) res.send(err);
+    return res.json(results);
   });
 });
 
--- react-app/src/components/admin/AdminAbout.js
+++ react-app/src/components/admin/AdminAbout.js
@@ -1,118 +1,111 @@
+// Class AdminAbout
 import React, { Component } from "react";
 class AdminAbout extends Component {
-    state = {
-        vision_txt: '',
-    }
+  state = {
+    vision_txt: "",
+    organization_txt: "",
+    address: ""
+  };
 
-    componentDidMount() {
-        this.setState({ ...this.state } = this.props.about)
-    }
-    handleSubmit = (e) => {
-        e.preventDefault()
-        let body = {
-            vision_txt: this.state.vision_txt
-        }
-        console.log(body);
-        /* fetch(`http://localhost:5000/venues/update`, {
-           method: 'POST',
-           headers: { 'Content-Type': 'application/json' },
-           body: JSON.stringify(body)
-         })
-           .then(_ => {
-             this.setState({ status: 'edited' })
-             this.refs.address.innerHTML = this.state.address
-             this.refs.capacity.innerHTML = this.state.capacity
-             this.refs.addressIcon.innerHTML = ""
-             this.refs.capacityIcon.innerHTML = ""
-           })
-           .catch(err => console.log(err))
-       }
-       handleChange = (e) => {
-         this.setState({ status: 'editing' })
-         switch (e.target.name) {
-           case 'address':
-             this.setState({ address: e.target.value })
-             this.refs.addressIcon.innerHTML = "&#9998;"
-             break;
-           case 'capacity':
-             this.setState({ capacity: e.target.value })
-             this.refs.capacityIcon.innerHTML = "&#9998;"
-             break;
-           default:*/
-    }
-    handleChange = (e) => {
-        switch (e.target.name) {
-            case 'vision_txt':
-                this.setState({ vision_txt: e.target.value })
-                //console.log(e.target.value);
-                //console.log(this.state.vision_txt);
-                break;
-            case 'dateHeader_txt':
-                this.setState({ dateHeader_txt: e.target.value })
-                //console.log(e.target.value);
-                //console.log(this.props.dateHeader_txt);
-                break;
-            default:
-        }
-    }
+  componentDidMount() {
+    // sets the props sent from parent 'AdminGeneral' as state
+    this.setState(({ ...this.state } = this.props.about));
+  }
 
-
-    render() {
-        return (
-            <React.Fragment>
-                <div className="elementCardAdmin row">
-                    <p className="col-lg-10">
-                        <span className="smallHeading">
-                            Visjon, organisasjon og kontaktadresse
-                    </span>
-                    </p>
-                    <div className="col-lg-2">
-                        <button
-                            className="btn  btn-secondary btnInElementAdmin btn-sm"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#staticTextForm"
-                            aria-expanded="false"
-                            aria-controls="staticTextForm"
-                        >
-                            Rediger
-                    </button>
-                    </div>
-                </div>
-                <div className="editScheduleItem collapse" id="staticTextForm">
-                    <form className="col-md-8 col-lg-6" onSubmit={this.handleSubmit}>
-                        <div className="form-group">
-                            <label>Visjon</label>
-                            <textarea
-                                className="form-control"
-                                defaultValue={this.props.about.vision_txt}
-                                name="vision_txt"
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Organisasjon</label>
-                            <textarea
-                                className="form-control"
-                                defaultValue={this.props.about.organization_txt}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Kontakt Adresse</label>
-                            <input
-                                className="form-control"
-                                defaultValue={this.props.about.address}
-                            />
-                        </div>
-                        <button type="submit" className="btn btn-info btn-sm">
-                            Lagre
-                    </button>
-                    </form>
-                </div>
-
-            </React.Fragment>
-        );
+  // function for when submit button has been clicked
+  handleSubmit = e => {
+    e.preventDefault();
+    // saves the new changes in object 'body'
+    let body = {
+      vision_txt: this.state.vision_txt,
+      organization_txt: this.state.organization_txt,
+      address: this.state.address
+    };
+    // sends 'body'-object to about/frontpageUpdate to update the database
+    fetch(`http://localhost:5000/about/frontpageUpdate`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    }).catch(err => console.log(err));
+  };
+  // function for when fields have been changed
+  handleChange = e => {
+    // checks name of target
+    switch (e.target.name) {
+      //if name equals 'vision_txt'..
+      case "vision_txt":
+        // .. it sets the value of target in state
+        this.setState({ vision_txt: e.target.value });
+        break;
+      case "organization_txt":
+        this.setState({ organization_txt: e.target.value });
+        break;
+      case "address":
+        this.setState({ address: e.target.value });
+        break;
+      default:
     }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="elementCardAdmin row">
+          <p className="col-lg-10">
+            <span className="smallHeading">
+              Visjon, organisasjon og kontaktadresse
+            </span>
+          </p>
+          <div className="col-lg-2">
+            <button
+              className="btn  btn-secondary btnInElementAdmin btn-sm"
+              type="button"
+              data-toggle="collapse"
+              data-target="#staticTextForm"
+              aria-expanded="false"
+              aria-controls="staticTextForm"
+            >
+              Rediger
+            </button>
+          </div>
+        </div>
+        <div className="editScheduleItem collapse" id="staticTextForm">
+          <form className="col-md-8 col-lg-6" onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label>Visjon</label>
+              <textarea
+                className="form-control"
+                defaultValue={this.props.about.vision_txt}
+                name="vision_txt"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Organisasjon</label>
+              <textarea
+                className="form-control"
+                defaultValue={this.props.about.organization_txt}
+                name="organization_txt"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Kontakt Adresse</label>
+              <input
+                className="form-control"
+                defaultValue={this.props.about.address}
+                name="address"
+                onChange={this.handleChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-info btn-sm">
+              Lagre
+            </button>
+          </form>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default AdminAbout;
