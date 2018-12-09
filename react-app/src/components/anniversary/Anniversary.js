4e7320c78af049a1b408da7044cc6c992c0f4b32
ragnals <ragnals@stud.ntnu.no>
2018-12-09 18:26:53 +0100

    jubileum

--- react-app/src/components/anniversary/Anniversary.js
+++ react-app/src/components/anniversary/Anniversary.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import Timeline from "./Timeline"
@@ -7,28 +7,30 @@ import "../../styles/anniversary.css"
 class Anniversary extends Component {
   state = {}
 
-  componentDidMount(){
+  componentDidMount() {
     console.log("We should loot some more info here I guess")
   }
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <>
         <Navbar />
         <div className="container">
           <div className="vh-85">
-            <h1 className="centerH">Drammen Sacred Music Festival fyller 10 år!</h1>
+            <h2 className="pageTitle">Drammen Sacred Music Festival fyller 10 år!</h2>
+            <hr className="hrHeight" />
             <article>
-              <p>Not actually sure if text goes here, maybe big dick image or collage video or smth. idkwat</p>
-              <p>Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text. Here goes super cool ultra text.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </article>
             <Timeline />
           </div>
         </div>
         <Footer />
       </>
-     );
+    );
   }
 }
- 
+
 export default Anniversary;
